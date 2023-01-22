1eb333c5c43808175aa00e6237d43a992388a655
alexlsiva7 <alex.lsilva154@gmail.com>
2023-01-22 16:41:53 +0000

    upload do script de verificação

---
+++ script_verifica_apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script que verifica o status do serviço httpd e salva o resultado em um arquivo no diretório /mnt/nfs/alex
+# Autor: Alex Lopes
+# Data: 22/01/2023
+
+DATA=$(date +%d/%m/%Y)
+HORA=$(date +%H:%M:%S)
+SERVICO="httpd"
+STATUS=$(systemctl is-active $SERVICO)
+
+if [ $STATUS == "active" ]; then
+    MENSAGEM="O $SERVICO está ONLINE"
+    echo "$DATA $HORA - $SERVICO - active - $MENSAGEM" >> /tmp/alexlopes/online.txt
+else
+    MENSAGEM="O $SERVICO está offline"
+    echo "$DATA $HORA - $SERVICO - inactive - $MENSAGEM" >> /tmp/alexlopes/offline.txt
+fi
